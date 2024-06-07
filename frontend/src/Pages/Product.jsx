539d01236418313beab35c7e2e274afd734aac59
Saidul Islam Rajib <saidul.is.rajib@gmail.com>
2024-06-08 02:36:19 +0600

    udpated component

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { HomeContext } from '../Context/HomeContext'
 import { useParams } from 'react-router-dom'
 import { Breadcrum } from '../Components/Breadcrums/Breadcrum'
+import { ProductDisplay } from '../Components/ProductDisplay/ProductDisplay'
 
 export const Product = () => {
   const {all_product} = useContext(HomeContext)
@@ -11,6 +12,7 @@ export const Product = () => {
   return (
     <div>
       <Breadcrum product={product} />
+      <ProductDisplay product={product} />
     </div>
   )
 }
