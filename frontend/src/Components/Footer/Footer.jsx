b3601d11b3610921328f6dad793b55339d437c48
Saidul Islam Rajib <saidul.is.rajib@gmail.com>
2024-06-09 22:29:02 +0600

    responsive footer

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -8,29 +8,29 @@ export const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <div className='footer'>
-        <div className="footer-logo">
-            <img src={footer_logo} alt="" />
-            <p>Sober</p>
+      <div className="footer-logo">
+        <img src={footer_logo} alt="" />
+        <p>Sober</p>
+      </div>
+      <ul className="footer-links">
+        <li>Company</li>
+        <li>Products</li>
+        <li>Offices</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+      <div className="footer-social-icon">
+        <div className="footer-icons-container">
+          <img src={instragram_icon} alt="" />
         </div>
-        <ul className="footer-links">
-            <li>Company</li>
-            <li>Products</li>
-            <li>Offices</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-        <div className="footer-social-icon">
-            <div className="footer-icons-container">
-                <img src={instragram_icon} alt="" />
-            </div>
-            <div className="footer-icons-container">
-                <img src={whatsapp_icon} alt="" />
-            </div>
-        </div>        
-        <div className="footer-copyright">
-            <hr />
-            <p>All rights reserved by Rajib-{currentYear}</p>
+        <div className="footer-icons-container">
+          <img src={whatsapp_icon} alt="" />
         </div>
+      </div>
+      <div className="footer-copyright">
+        <hr />
+        <p>All rights reserved by Rajib-{currentYear}</p>
+      </div>
     </div>
   )
 }
