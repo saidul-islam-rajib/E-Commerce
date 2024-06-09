0617c7074046365ee24f32f63e3183ba838eaf15
Saidul Islam Rajib <saidul.is.rajib@gmail.com>
2024-06-09 22:24:39 +0600

    Subscription section make responsive for all devices

--- frontend/src/Components/NewsLetter/NewsLetter.jsx
+++ frontend/src/Components/NewsLetter/NewsLetter.jsx
@@ -4,12 +4,12 @@ import './NewsLetter.css'
 export const NewsLetter = () => {
   return (
     <div className='newsletter'>
-        <h1>Get Exclusive Offers On Your Email</h1>
-        <p>Subscribe to our newsletter and stay updated</p>
-        <div>
-            <input type="email" placeholder='saidul.is.rajib@gmail.com' />
-            <button>Subscribe</button>
-        </div>
+      <h1>Get Exclusive Offers On Your Email</h1>
+      <p>Subscribe to our newsletter and stay updated</p>
+      <div>
+        <input type="email" placeholder='saidul.is.rajib@gmail.com' />
+        <button>Subscribe</button>
+      </div>
     </div>
   )
 }
